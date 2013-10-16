30c8f7494c4784f79f557cb32f009d3eef208e95
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2013-10-16 22:00:45 +0200

    extracted spider into separate class

---
+++ spider.rb
@@ -0,0 +1,85 @@
+require 'httparty'
+require 'nokogiri'
+require 'sequel'
+require 'set'
+require 'uri'
+require 'colored'
+
+class Spider
+  attr_accessor :resources, :visited_links, :unvisited_links, :bad_uris
+
+  def initialize(start_page, host)
+    @start_page = start_page
+    @host = host
+
+    @visited_links = Set.new
+    @unvisited_links = Set.new
+    @bad_uris = Set.new
+    @resources = Set.new
+  end
+
+  def run
+    visit!(@start_page)
+
+    until @unvisited_links.empty?
+      Set.new(@unvisited_links).each{|link| visit!(link)}
+    end
+  end
+
+  def visit!(page)
+    @visited_links.add(page)
+
+    response = HTTParty.get(page)
+    doc = Nokogiri::HTML(response.body)
+
+    links = extract_resources(doc, 'a[href]', 'href')
+
+    scripts = extract_resources(doc, 'script[src]', 'src')
+    stylesheets = extract_resources(doc, 'link[rel=stylesheet]', 'href')
+    images = extract_resources(doc, 'img[src]', 'src')
+
+    @resources.merge(scripts + stylesheets + images)
+
+    @unvisited_links.merge(links)
+    @unvisited_links.subtract(@visited_links)
+
+    puts "visited: #{@visited_links.count}".green
+    puts "unvisited: #{@unvisited_links.count}".green
+  end
+
+  def extract_resources(document, selector, attribute)
+    document.css(selector).map{|e| e[attribute]}.select{|resource| filter_by_host(resource)}
+  end
+
+  def filter_by_host(resource)
+    begin
+      uri = URI(resource)
+    rescue URI::InvalidURIError => error
+      @bad_uris.add(resource)
+      puts "#{error.message}".yellow
+      return false
+    end
+
+    uri.host == @host
+  end
+
+  def persist!
+    db = Sequel.sqlite("#{@host}.sqlite3")
+
+    db.create_table :resources do
+      primary_key :id
+      String :url, :unique => true, :null => false
+    end
+
+    dataset = db[:resources]
+    @resources.each{|e| dataset.insert(:url => e)}
+
+    db.create_table :bad_uris do
+      primary_key :id
+      String :url, :unique => true, :null => false
+    end
+
+    dataset = db[:bad_uris]
+    @bad_uris.each{|e| dataset.insert(:url => e)}
+  end
+end
